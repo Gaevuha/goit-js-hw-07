471ec8934dc4b46a34af7ca4cec951917c201df6
gaevuha <gaevuha@gmail.com>
2025-04-08 10:59:38 +0300

    add task-5 on  hw-07

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeBackground = document.querySelector("body");
+
+const changeColor = document.querySelector('.change-color');
+
+const textColor = document.querySelector('.color');
+
+changeColor.addEventListener("click", () => {
+
+  const randomColor = getRandomHexColor();
+
+  changeBackground.style.backgroundColor = randomColor;
+  
+  textColor.textContent = randomColor;
+
+});
